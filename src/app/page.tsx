e469981591453779440500cc377736fa86f3e197
Ademir Gustavo <ademirgus@gmail.com>
2023-05-22 22:21:13 -0300

    init:components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Footer } from '@/components/Footer'
+import { Sidebar } from '@/components/Sidebar'
 import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize2} from 'lucide-react'
 import Image from 'next/image'
 
@@ -7,35 +9,7 @@ export default function Home() {
   return (
     <main className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <div className="flex items-center gap-2">
-            <div className="w-3 h-3 bg-red-500 rounded-full" />
-            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
-            <div className="w-3 h-3 bg-green-500 rounded-full" />
-        </div>
-      <nav className="space-y-5 mt-10">
-        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-          <HomeIcon />
-          Home
-        </a>
-        <a href="" className="flex items-center gap-3 text-sm  font-semibold text-zinc-200">
-          <Search />
-          Search
-        </a>
-        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-          <Library />
-          Your Library
-        </a>
-      </nav>
-
-        <nav className="mt-6 pt-6 border-t botder-zinc-800 flex flex-col gap-3">
-          <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Hot Hits Brasil</a>
-          <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Errejota cult Drill</a>
-          <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Melhor Opção</a>
-          <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Não Estou bem</a>
-          <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Techno</a>
-        </nav>
-      </aside>
+        <Sidebar />
       <main className="flex-1 p-6">
         <div className="flex items-center gap-4">
           <button>
@@ -130,48 +104,7 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <Image src="/album.jpg" width={56} height={56} alt="Capa Astroworld" />
-          <div className="flex flex-col">
-            <strong className="font-normal">WAKE UP</strong>
-            <span className="text-xs text-zinc-400">Travis Scott</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <div className="flex items-center gap-6">
-            <Shuffle size={20} className="text-zinc-200" />
-            <SkipBack  size={20} className="text-zinc-200"/>
-
-            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
-              <Play />
-            </button>
-
-            <SkipForward size={20} className="text-zinc-200" />
-            <Repeat size={20} className="text-zinc-200" />
-          </div>
-          <div className="flex items-center gap-2">
-            <span className="text-sm text-zinc-400">0:31</span>
-            <div className="h-1 rounded-full w-96 bg-zinc-600">
-              <div className="bg-zinc-200 w-40 h-1 rounded-full">
-              </div>
-            </div>
-            <span className="text-sm text-zinc-400">2:14</span>
-          </div>
-        </div>
-        <div className="flex items-center gap-4">
-          <Mic2 size={20} />
-          <LayoutList size={20} />
-          <Laptop2 size={20}/>
-          <div className="flex items-center gap-2">
-            <Volume size={20}/>
-            <div className="h1 rounded-full w-24 bg-zinc-600">
-              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
-            </div>
-          </div>
-          <Maximize2 size={20}/>
-        </div>
-      </footer>
+      <Footer />
     </main>
   )
 }
